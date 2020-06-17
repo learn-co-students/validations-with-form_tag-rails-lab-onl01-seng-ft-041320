cce0de618fec0744d9c6e525e4d9b1cf360236df
nolyoi <nolan@moonship.cc>
2020-06-16 23:51:16 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  [:authors, :posts].each do |route|
+    resources route, only: [:show, :new, :edit, :create, :update]
+  end
 end
